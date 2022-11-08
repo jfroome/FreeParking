4c8c5c908bd562c7baa2cf3c400a251431d36355
jfroome <froo0014@algonquinlive.com>
2022-11-08 13:25:11 -0500

    dotenv implemented

--- tests/Valprkg.js
+++ tests/Valprkg.js
@@ -1,17 +1,21 @@
+
+
+
 describe('valprkg', function() {
     it('Submit parking form', function(browser) {
+      require('dotenv').config()
       browser
         .navigateTo('http://www.valprkg.com/')
         .waitForElementVisible('*[type="TEXT"]')
-        .sendKeys('*[type="TEXT"]', ['498'])
+        .sendKeys('*[type="TEXT"]', [`${process.env.BC}`])
         .waitForElementVisible('*[type="SUBMIT"')
         .click('*[type="SUBMIT"]')
         .waitForElementVisible('input[name="name"]')
-        .sendKeys('input[name="name"]', '49800')
+        .sendKeys('input[name="name"]', `${process.env.CC}`)
         .waitForElementVisible('input[name="plate"]')
-        .sendKeys('input[name="plate"]', 'K1K1K1')
+        .sendKeys('input[name="plate"]', `${process.env.PLATENUM}`)
         .waitForElementVisible('select[name="provorstate"]')
-        .click('select[name="provorstate"] option[value="QC"]')
+        .click(`select[name="provorstate"] option[value="${process.env.PROV}"]`)
         .waitForElementVisible('input[name="punits"]')
         .sendKeys('input[name="punits"]', '1')
         .click('*[type="SUBMIT"]')
